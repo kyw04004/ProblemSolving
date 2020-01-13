8fdc7f2cc59d4b7715f4043ac05028e3f0b10b63
kyw04004 <kyw04004@naver.com>
2020-01-14 04:19:01 +0900

    Create 1992_쿼드트리.cpp

---
+++ 1992_쿼드트리.cpp
@@ -0,0 +1,41 @@
+#include<cstring>
+#include<string>
+#include<algorithm>
+#include<iostream>
+#include<stdio.h>
+#include<vector>
+#include<cmath>
+using namespace std;
+int arr[65][65];
+void go(int n, int x, int y)
+{
+	int s = arr[x][y];
+	for (int i = x; i < x + n; i++)
+		for (int j = y; j < y + n; j++)
+		{
+			if (arr[i][j] != s)
+			{
+				printf("(");
+				go(n / 2, x, y);
+				go(n / 2, x, y + n / 2);
+				go(n / 2, x + n / 2, y);
+				go(n / 2, x + n / 2, y + n / 2);
+				printf(")");
+				return;
+			}
+		}
+	if (!s)
+		printf("0");
+	else
+		printf("1");
+}
+int main()
+{
+	int n;
+	cin >> n;
+	for (int i = 1; i <= n; i++)
+		for (int j = 1; j <= n; j++)
+			scanf("%1d", &arr[i][j]);
+	go(n, 1, 1);
+	return 0;
+}
