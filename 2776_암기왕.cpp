f04a1ffcb978918bdf0b3234688c137d15bfa270
kyw04004 <kyw04004@naver.com>
2020-01-29 18:32:40 +0900

    Create 2776_암기왕.cpp

---
+++ 2776_암기왕.cpp
@@ -0,0 +1,45 @@
+#include<stdio.h>
+#include<iostream>
+#include<cstring>
+#include<string>
+#include<algorithm>
+#include<cmath>
+#include<vector>
+#include<queue>
+#include<functional>
+#include<stack>
+using namespace std;
+int main()
+{
+	int t;
+	scanf("%d", &t);
+	while (t--)
+	{
+		int n, m;
+		int com[1000001] = { 0 };
+		scanf("%d", &n);
+		for (int i = 0; i < n; i++)
+			scanf("%d", &com[i]);
+		sort(com, com + n);
+		scanf("%d", &m);
+		for (int i = 0; i < m; i++)
+		{
+			int a;
+			scanf("%d", &a);
+			int left = 0, right = n - 1;
+			while (left <= right)
+			{
+				int mid = (left + right) / 2;
+				if (a > com[mid])
+					left = mid + 1;
+				else
+					right = mid - 1;
+			}
+			if (com[left] == a)
+				printf("1\n");
+			else
+				printf("0\n");
+		}
+	}
+	return 0;
+}
