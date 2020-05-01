84a46e220e97a71eac865aae54b5dfab51447faf
kyw04004 <kyw04004@naver.com>
2020-05-01 20:58:23 +0900

    Create 2417_정수 제곱근.cpp

---
+++ 2417_정수 제곱근.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+#include<cstring>
+#include<string>
+#include<algorithm>
+#include<stdio.h>
+#include<math.h>
+using namespace std;
+int main()
+{
+	long long n;
+	cin >> n;
+	printf("%lld", (long long)ceil(sqrt(n)));
+	return 0;
+}
