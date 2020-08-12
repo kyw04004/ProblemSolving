533b7eae47eeafa8cbe93c91db1d586e9b4b8c3c
kyw04004 <kyw04004@naver.com>
2020-08-12 20:34:32 +0900

    Update 11657_타임머신.cpp

--- 11657_타임머신.cpp
+++ 11657_타임머신.cpp
@@ -1,46 +1,57 @@
 #include<iostream>
-#include<string>
 #include<cstring>
+#include<string>
 #include<algorithm>
-#include<queue>
 #include<vector>
+#include<queue>
 using namespace std;
-bool cycle;
-int n, m, a, b, c, dist[501];
-vector<vector<pair<int,int>>> v(501);
-int main()
-{
-	scanf("%d %d", &n, &m);
-	for (int i = 1; i <= m; i++)
-	{
-		scanf("%d %d %d", &a, &b, &c);
-		v[a].push_back({ b,c });
-	}
-	for (int i = 1; i <= n; i++)
-		dist[i] = 1e9;
-	dist[1] = 0;
-	bool cycle = 0;
-	for (int k = 1; k <= n; k++)
-		for (int i = 1; i <= n; i++)
-			for (int j = 0; j < v[i].size(); j++)
-			{
-				int next = v[i][j].first;
-				int nextcost = v[i][j].second;
-				if (dist[i] != 1e9&&dist[next] > dist[i] + nextcost)
-				{
-					dist[next] = dist[i] + nextcost;
-					if (k == n) cycle = 1;
+typedef long long ll;
+ll t, n, m, w, dist[505], inQ[505], cycle[505], chk, visit[505];
+queue <ll> q;
+vector <pair<ll, ll> > v[505];
+void spfa(int src) {
+	dist[src] = 0;
+	inQ[src] = 1;
+	visit[src] = 1;
+	q.push(src);
+	cycle[src]++;
+	while (!q.empty()) {
+		ll now = q.front();
+		visit[now] = 1;
+		q.pop();
+		inQ[now] = 0;
+		for (int i = 0; i < v[now].size(); i++) {
+			ll next = v[now][i].first;
+			ll cost = v[now][i].second;
+			if (visit[now] && dist[next] > dist[now] + cost) {
+				dist[next] = dist[now] + cost;
+				if (!inQ[next]) {
+					cycle[next]++;
+					if (cycle[next] >= n) {
+						chk = 1;
+						return;
+					}
+					q.push(next);
+					inQ[next] = 1;
 				}
 			}
-	if (cycle == 1) 
-		printf("-1");
-	else
-		for (int i = 2; i <= n; i++)
-		{
-			if (dist[i] == 1e9)
-				printf("-1\n");
-			else
-				printf("%d\n", dist[i]);
 		}
+	}
+}
+int main() {
+	cin >> n >> m;
+	for (int i = 0; i < m; i++) {
+		ll a, b, c;
+		cin >> a >> b >> c;
+		v[a].push_back({ b,c });
+	}
+	for (int i = 1; i <= n; i++) dist[i] = 1e18;
+	spfa(1);
+	if (chk == 1) cout << "-1\n";
+	else {
+		for (int i = 2; i <= n; i++)
+			if (!visit[i]) printf("-1\n");
+			else printf("%lld\n", dist[i]);
+	}
 	return 0;
 }
