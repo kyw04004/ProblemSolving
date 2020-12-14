58cad7a320bae694ce246f50f98ea368b4b18527
kyw04004 <kyw04004@naver.com>
2020-12-15 04:49:23 +0900

    Update 2611_자동차경주.cpp

--- 2611_자동차경주.cpp
+++ 2611_자동차경주.cpp
@@ -1,56 +1,41 @@
 #include<iostream>
-#include<stdlib.h>
 #include<cstring>
 #include<string>
 #include<algorithm>
 #include<queue>
 #include<functional>
-#include<stack>
 using namespace std;
-priority_queue <pair<int,int>, vector<pair<int, int>> ,less<pair<int, int>>> pq;
-vector<pair<int,int>> v[1001];
-stack <int> s;
-int n, m, dist[1001], path[1001];
+vector<pair<int, int>> v[1001];
+int n, m ,dp[1001], path[1001];
+int go(int now) {
+	if (dp[now] != -1 && now == 1) return 0;
+	int& ret = dp[now];
+	if (ret != -1) return ret;
+	ret = 0;
+	for (int i = 0; i < v[now].size(); i++) {
+		int next = v[now][i].first;
+		int nextcost = v[now][i].second;
+		int com = go(next) + nextcost;
+		if (com > ret) ret = com, path[now] = next;
+	}
+	return ret;
+}
 int main()
 {
+	memset(dp, -1, sizeof(dp));
 	cin >> n >> m;
 	for (int i = 0; i < m; i++) {
 		int a, b, c;
 		cin >> a >> b >> c;
 		v[a].push_back({ b,c });
 	}
-	for (int i = 0; i < v[1].size(); i++)
-	{
-		int next = v[1][i].first;
-		int nextcost = v[1][i].second;
-		dist[next] = nextcost;
-		pq.push({ dist[next], next });
-	}
-	while (!pq.empty()) {
-		int cost = pq.top().first;
-		int here = pq.top().second;
-		pq.pop();
-		for (int i = 0; i < v[here].size(); i++) {
-			int next = v[here][i].first;
-			int nextcost = v[here][i].second;
-			if (dist[next] < dist[here] + nextcost) {
-				path[next] = here;
-				dist[next] = dist[here] + nextcost;
-				if(next != 1) pq.push({ dist[next], next });
-			}
-		}
-	}
-	printf("%d\n", dist[1]);
-	int now = 1;
+	printf("%d\n", go(1));
+	int here = 1;
+	printf("%d ", here);
 	while (1) {
-		s.push(now);
-		now = path[now];
-		if (now == 0) break;
-	}
-	s.push(1);
-	while (!s.empty()) {
-		printf("%d ", s.top());
-		s.pop();
+		printf("%d ", path[here]);
+		here = path[here];
+		if (here == 1) break;
 	}
 	return 0;
 }
