e2810ad70f009f9eb78c8468b364c863c756977a
kyw04004 <kyw04004@naver.com>
2021-01-05 23:20:33 +0900

    Create 9656_돌 게임 2.cpp

---
+++ BOJ/9656_돌 게임 2.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+#include<cstring>
+#include<string>
+#include<algorithm>
+#include<vector>
+#include<cmath>
+#include<queue>
+using namespace std;
+typedef long long ll;
+int main() {
+	cin.tie(NULL);
+	cout.tie(NULL);
+	ios_base::sync_with_stdio(false);
+	int n; cin >> n;
+	if (n % 2 == 1) cout << "CY";
+	else cout << "SK";
+	return 0;
+}
