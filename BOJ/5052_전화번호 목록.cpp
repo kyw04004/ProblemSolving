b759e2c9cbeca740f68453b8cd0f218d17e1e7e2
kyw04004 <kyw04004@naver.com>
2021-01-11 12:02:38 +0900

    Update 5052_전화번호 목록.cpp

--- BOJ/5052_전화번호 목록.cpp
+++ BOJ/5052_전화번호 목록.cpp
@@ -2,46 +2,62 @@
 #include<cstring>
 #include<string>
 #include<algorithm>
+#include<vector>
+#include<cmath>
+#include<queue>
+#include<functional>
+#include<stack>
 using namespace std;
-bool isConsistent = true;
+typedef long long ll;
 struct Trie {
-	bool isWord, existChild;
-	Trie* childTrie[10];
+	bool finish;
+	Trie* child[10];
 	Trie() {
-		isWord = false;
-		existChild = false;
-		for (int i = 0; i < 10; i++) childTrie[i] = nullptr;
+		fill(child, child + 10, nullptr);
+		finish = false;
 	}
 	~Trie() {
 		for (int i = 0; i < 10; i++)
-			if (childTrie[i])
-				delete childTrie[i];
+			if (child[i]) delete child[i];
 	}
-	void insert(char* word) {
-		if (*word == '\0') isWord = true;
-		else {
-			int next = *word - '0';
-			if (!childTrie[next]) childTrie[next] = new Trie;
-			existChild = true;
-			childTrie[next]->insert(word + 1);
+	void insert(const char* key) {
+		if (*key == NULL) {
+			finish = true;
+			return;
 		}
-		if (isWord && existChild) isConsistent = false;
+		else {
+			int next = *key - '0';
+			if (child[next] == NULL) child[next] = new Trie;
+			child[next]->insert(key + 1);
+		}
+	}
+	bool exist(const char* key) {
+		if (*key == NULL) return false;
+		if (finish) return true;
+		int next = *key - '0';
+		if (child[next] == NULL) return false;
+		return child[next]->exist(key + 1);
 	}
 };
 int main() {
-	int t, n;
-	cin >> t;
-	for (int i = 0; i < t; i++) {
-		isConsistent = true;
+	cin.tie(NULL);
+	cout.tie(NULL);
+	ios_base::sync_with_stdio(false);
+	int t; cin >> t;
+	while (t--) {
+		int c = 1;
+		int n; cin >> n;
+		char input[10005][11];
 		Trie* trie = new Trie;
-		cin >> n;
-		for (int j = 0; j < n; j++) {
-			char word[11];
-			cin >> word;
-			trie->insert(word);
+		for (int i = 0; i < n; i++) {
+			cin >> input[i];
+			trie->insert(input[i]);
 		}
-		if (isConsistent) cout << "YES\n";
-		else cout << "NO\n";
+		for (int i = 0; i < n; i++)
+			if (trie->exist(input[i])) c = 0;
 		delete trie;
+		if (c) cout << "YES\n";
+		else cout << "NO\n";
 	}
+	return 0;
 }
