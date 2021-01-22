0b9af42da4c0c2ff488d04d603097fdfb68ed066
kyw04004 <kyw04004@naver.com>
2021-01-22 20:18:43 +0900

    Update 1074_Z.cpp

--- BOJ/1074_Z.cpp
+++ BOJ/1074_Z.cpp
@@ -3,19 +3,23 @@
 #include<string>
 #include<algorithm>
 using namespace std;
-int N, r, c,cnt;
+int N, r, c, cnt;
 void go(int x, int y, int Size) {
 	if (Size == 2) {
 		if (x == r && y == c) { cout << cnt; return; } cnt++;
 		if (x == r && y + 1 == c) { cout << cnt; return; } cnt++;
-		if (x + 1 == r && y  == c) { cout << cnt; return; } cnt++;
+		if (x + 1 == r && y == c) { cout << cnt; return; } cnt++;
 		if (x + 1 == r && y + 1 == c) { cout << cnt; return; } cnt++;
 		return;
 	}
-	go(x, y, Size / 2);
-	go(x, y + Size / 2, Size / 2);
-	go(x + Size / 2, y, Size / 2);
-	go(x + Size / 2, y + Size / 2, Size / 2);
+	if (x <= r && r < x + Size / 2 && y <= c && c < y + Size / 2) go(x, y, Size / 2);
+	else cnt += Size*Size/4;
+	if (x <= r && r < x + Size / 2 && y + Size / 2 <= c && c < y + Size) go(x, y + Size / 2, Size / 2);
+	else cnt += Size * Size / 4;
+	if (x + Size / 2 <= r && r < x + Size && y <= c && c < y + Size / 2) go(x + Size / 2, y, Size / 2);
+	else cnt += Size * Size / 4;
+	if (x + Size / 2 <= r && r < x + Size && y + Size / 2 <= c && c < y + Size) go(x + Size / 2, y + Size / 2, Size / 2);
+	else cnt += Size * Size / 4;
 }
 int main() {
 	cin >> N >> r >> c;
