4978aec49a2164da5817aa25a33c1e258e9fa724
kyw04004 <kyw04004@naver.com>
2021-01-27 22:26:21 +0900

    Update 1761_정점들의 거리.cpp

--- BOJ/1761_정점들의 거리.cpp
+++ BOJ/1761_정점들의 거리.cpp
@@ -1,72 +1,62 @@
+#include<iostream>
 #include<cstring>
 #include<string>
 #include<algorithm>
-#include<iostream>
-#include<stdio.h>
 #include<vector>
 #include<cmath>
 #include<queue>
 #include<functional>
+#include<stack>
+#include<map>
+#include<bitset>
 using namespace std;
-vector<vector<pair<int,int>>> v(40001);
-int depth[40001];
-int par[40001][20];
-int dist[40001];
-void dfs(int now, int level)
-{
-	depth[now] = level;
-	for (int i = 0; i < v[now].size(); i++)
-	{
+typedef long long ll;
+int n, m, depth[40005], parent[40005][17],dist[40005];
+vector<pair<int,int>> v[40005];
+void dfs(int now, int dep) {
+	depth[now] = dep;
+	for (int i = 0; i < v[now].size(); i++) {
 		int next = v[now][i].first;
 		int nextcost = v[now][i].second;
-		if (depth[next]) continue;
-		dist[next] = dist[now] + nextcost;
-		dfs(next, level + 1);
-		par[next][0] = now;
+		if (!depth[next]) {
+			dist[next] = dist[now] + nextcost;
+			parent[next][0] = now;
+			dfs(next, dep + 1);
+		}
 	}
 }
-int LCA(int a, int b)
-{
-	if (depth[a] < depth[b]) swap(a, b);
-	int dif = depth[a] - depth[b];
-	for (int i = 0; i <= 16; i++)
-		if (dif & (1 << i))
-			a = par[a][i];
-	if (a != b)
-	{
-		for (int i = 16; i >= 0; i--)
-			if (par[a][i] && par[a][i] != par[b][i])
-			{
-				a = par[a][i];
-				b = par[b][i];
-			}
-		a = par[a][0];
-	}
-	return a;
+int LCA(int a, int b) {
+	if (depth[a] > depth[b]) swap(a, b);
+	for (int i = 16; i >= 0; i--)
+		if (depth[a] <= depth[parent[b][i]])
+			b = parent[b][i];
+	if (a == b) return a;
+	for (int i = 16; i >= 0; i--)
+		if (parent[a][i] != parent[b][i]) {
+			a = parent[a][i];
+			b = parent[b][i];
+		}
+	return parent[a][0];
 }
-int main()
-{
-	int n, m;
-	scanf("%d", &n);
-	for (int i = 1; i < n; i++)
-	{
-		int a, b, c;
-		scanf("%d %d %d", &a, &b, &c);
+int main() {
+	cin.tie(NULL);
+	cout.tie(NULL);
+	ios_base::sync_with_stdio(false);
+	cin >> n;
+	for (int i = 1; i < n; i++) {
+		int a, b, c; cin >> a >> b >> c;
 		v[a].push_back({ b,c });
 		v[b].push_back({ a,c });
 	}
 	dfs(1, 1);
-	for (int i = 1; i <= 16; i++)
-		for (int j = 1; j <= n; j++)
-			par[j][i] = par[par[j][i - 1]][i - 1];
-	scanf("%d", &m);
-	for (int i = 1; i <= m; i++)
-	{
-		int a, b, p;
-		scanf("%d %d", &a, &b);
-		p = LCA(a, b);
-		int sum = dist[a] + dist[b] - 2*dist[p];
-		printf("%d\n", sum);
+	for (int j = 1; j <= 16; j++)
+		for (int i = 1; i <= n; i++)
+			parent[i][j] = parent[parent[i][j - 1]][j - 1];
+	cin >> m;
+	for (int i = 1; i <= m; i++) {
+		int a, b; cin >> a >> b;
+		int lca = LCA(a, b);
+		cout << dist[a] + dist[b] - 2*dist[lca] << '\n';
 	}
 	return 0;
 }
