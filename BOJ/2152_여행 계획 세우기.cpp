828c2c7d144284fe9f9b330d6d32c5d9615d79b4
kyw04004 <kyw04004@naver.com>
2021-02-05 09:45:14 +0900

    Update 2152_여행 계획 세우기.cpp

--- BOJ/2152_여행 계획 세우기.cpp
+++ BOJ/2152_여행 계획 세우기.cpp
@@ -1,110 +1,79 @@
-#include<stdio.h>
+//SCC -  Tarjan
 #include<iostream>
 #include<cstring>
 #include<string>
 #include<algorithm>
-#include<cmath>
 #include<vector>
+#include<cmath>
 #include<queue>
 #include<functional>
 #include<stack>
+#include<map>
+#include<bitset>
 using namespace std;
-vector<vector<int>> v;
-vector<vector<int>> rv;
-vector<vector<int>> scc;
-vector<vector<int>> big;
-vector<int> newscc;
-stack <int> s;
-vector <pair<int, int>> gan;
-queue <int> q;
-int V, E, S, T;
-int chk[10001], chk2[10001];
-int arr[10001], val[10001];
-int ans[10001], out[10001], go[10001];
-void dfs(int now) // 스택에 담기
-{
-	chk[now] = 1;
-	for (int i = 0; i < v[now].size(); i++)
-	{
+typedef long long ll;
+int n, m, s, t,S, schk[10005], scc[10005], cnt, cost[10005], dp[10005];
+vector<vector<int>> v, ans, sccv;
+vector<pair<int, int>> input;
+stack<int> st;
+int dfs(int now) {
+	st.push(now);
+	schk[now] = S++;
+	int parent = schk[now];
+	for (int i = 0; i < v[now].size(); i++) {
 		int next = v[now][i];
-		if (chk[next] == 0)
-			dfs(next);
+		if (schk[next] == -1) parent = min(parent, dfs(next));
+		else if (scc[next] == -1) parent = min(parent, schk[next]);
 	}
-	s.push(now);
+	if (parent == schk[now]) {
+		vector<int> temp;
+		while (1) {
+			int here = st.top();
+			st.pop();
+			scc[here] = cnt;
+			temp.push_back(here);
+			if (here == now) break;
+		}
+		ans.push_back(temp);
+		cnt++;
+	}
+	return parent;
 }
-void dfs2(int now) // scc 묶기
-{
-	chk2[now] = 1;
-	newscc.push_back(now);
-	for (int i = 0; i < rv[now].size(); i++)
-	{
-		int next = rv[now][i];
-		if (chk2[next] == 0)
-			dfs2(next);
-	}
-}
-int main()
-{
-	scanf("%d %d %d %d", &V, &E, &S, &T);
-	v.resize(V + 1);
-	rv.resize(V + 1);
-	big.resize(V + 1);
-	for (int i = 1; i <= E; i++)
-	{
-		int a, b;
-		scanf("%d %d", &a, &b);
-		gan.push_back({ a,b });
-		v[a].push_back(b);
-		rv[b].push_back(a);
-	}
-	for (int i = 1; i <= V; i++)
-		if (chk[i] == 0)
-			dfs(i);
-	while (!s.empty())
-	{
-		int now = s.top();
-		s.pop();
-		if (chk2[now] == 1)
-			continue;
-		newscc.clear();
-		dfs2(now);
-		scc.push_back(newscc);
-		for (int i = 0; i < newscc.size(); i++)
-			arr[newscc[i]] = scc.size();
-		val[scc.size()] = newscc.size();
-	}
-	for (int i = 0; i < gan.size(); i++)
-	{
-		if (arr[gan[i].first] == arr[gan[i].second])
-			continue;
-		big[arr[gan[i].first]].push_back(arr[gan[i].second]);
-		out[arr[gan[i].second]]++;
-	}
-	for (int i = 1; i <= scc.size(); i++) {
-		ans[i] = val[i];
-		if (out[i] == 0)
-			q.push(i);
-		if (i == arr[S])
-			go[i] = 1;
-	}
-	while (!q.empty()) {
-		int here = q.front();
-		q.pop();
-		for (int i = 0; i < big[here].size(); i++) {
-			int next = big[here][i];
-			if (go[here])
-			{
-				ans[next] = max(ans[next], ans[here] + val[next]);
-				go[next] = 1;
-			}
-			out[next] -= 1;
-			if (out[next] == 0)
-				q.push(next);
+void go(int now) {
+	for (int i = 0; i < sccv[now].size(); i++) {
+		int next = sccv[now][i];
+		if (dp[next] < dp[now] + cost[next]) {
+			dp[next] = dp[now] + cost[next];
+			go(next);
 		}
 	}
-	if (go[arr[T]])
-		printf("%d\n", ans[arr[T]]);
-	else
-		printf("0\n");
+}
+int main() {
+	cin.tie(NULL);
+	cout.tie(NULL);
+	ios_base::sync_with_stdio(false);
+	cin >> n >> m >> s >> t;
+	v.resize(n + 1);
+	memset(schk, -1, sizeof(schk));
+	memset(scc, -1, sizeof(scc));
+	for (int i = 0; i < m; i++) {
+		int a, b;
+		cin >> a >> b;
+		v[a].push_back(b);
+		input.push_back({ a,b });
+	}
+	for (int i = 1; i <= n; i++)
+		if (schk[i] == -1) dfs(i);
+	sccv.resize(cnt + 1);
+	for (int i = 0; i < cnt; i++) cost[i] = ans[i].size();
+	for (int i = 0; i < input.size(); i++) { 
+		int s = input[i].first;
+		int e = input[i].second;
+		if (scc[s] == scc[e]) continue;
+		sccv[scc[s]].push_back(scc[e]);
+	}
+	dp[scc[s]] = cost[scc[s]];
+	go(scc[s]);
+	cout << dp[scc[t]];
 	return 0;
 }
