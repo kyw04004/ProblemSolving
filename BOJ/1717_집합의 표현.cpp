4e1bf82b81f09f236049e7d9da92d9a76dd35106
kyw04004 <kyw04004@naver.com>
2021-02-15 10:37:51 +0900

    Update 1717_집합의 표현.cpp

--- BOJ/1717_집합의 표현.cpp
+++ BOJ/1717_집합의 표현.cpp
@@ -1,48 +1,51 @@
-#include<stdio.h>
-#include<stdlib.h>
-#include<math.h>
 #include<iostream>
 #include<cstring>
 #include<string>
 #include<algorithm>
-#include<functional>
-#include<queue>
 #include<vector>
+#include<cmath>
+#include<queue>
+#include<functional>
+#include<stack>
+#include<map>
+#include<bitset>
 using namespace std;
-int n, m, a, b, c, sum;
-int parent[1000005];
-typedef pair<int, pair<int, int>> P;
-priority_queue<P, vector<P>, greater<P>> pq;
-int find(int n)
-{
-	if (parent[n] < 0) return n;
-	parent[n] = find(parent[n]);
-	return parent[n];
+typedef long long ll;
+int n, m, parent[1000005];
+int find(int x) {
+	if (parent[x] == x) return x;
+	parent[x] = find(parent[x]);
+	return parent[x];
 }
-void merge(int a, int b, int c)
-{
-	a = find(a);
-	b = find(b);
-	if (a == b)
-	{
-		if (c == 1)
-			printf("YES\n");
-		return;
-	}
-	if (c == 1)
-		printf("NO\n");
-	if (c == 0)
-		parent[b] = a;
+void merge(int x, int y) {
+	x = find(x);
+	y = find(y);
+	if (x == y) return;
+	else parent[y] = x;
 }
-int main()
-{
-	scanf("%d%d", &n, &m);
-	for (int i = 0; i <= n; i++)
-		parent[i] = -1;
-	for (int i = 1; i <= m; i++)
-	{
-		scanf("%d%d%d", &a, &b, &c);
-		merge(b, c, a);
+bool isUnion(int x, int y) {
+	x = find(x);
+	y = find(y);
+	if (x == y) return true;
+	else return false;
+}
+int main() {
+	cin.tie(NULL);
+	cout.tie(NULL);
+	ios_base::sync_with_stdio(false);
+	cin >> n >> m;
+	for (int i = 1; i <= n; i++) parent[i] = i;
+	for (int i = 0; i < m; i++) {
+		int select, a, b;
+		cin >> select >> a >> b;
+		if (select == 1) {
+			if (isUnion(a, b)) cout << "YES\n";
+			else cout << "NO\n";
+		}
+		else {
+			if(isUnion(a, b)) continue;
+			else merge(a, b);
+		}
 	}
 	return 0;
 }
