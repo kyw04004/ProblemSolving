917d25520bb0d0ac2b49d2ca4863479d44300f8a
kyw04004 <kyw04004@naver.com>
2021-02-21 21:51:26 +0900

    Create 15651_N과 M (3).cpp

---
+++ BOJ/15651_N과 M (3).cpp
@@ -0,0 +1,35 @@
+#include<iostream>
+#include<cstring>
+#include<string>
+#include<algorithm>
+#include<vector>
+#include<cmath>
+#include<queue>
+#include<functional>
+#include<stack>
+#include<map>
+#include<bitset>
+using namespace std;
+typedef long long ll;
+int n, m;
+void go(vector<int> v, int index) {
+	if (v.size() == m) {
+		for (int i = 0; i < m; i++) cout << v[i] << ' ';
+		cout << '\n';
+		return;
+	}
+	for (int i = index; i <= n; i++) {
+		v.push_back(i);
+		go(v, index);
+		v.pop_back();
+	}
+}
+int main() {
+	cin.tie(NULL);
+	cout.tie(NULL);
+	ios_base::sync_with_stdio(false);
+	cin >> n >> m;
+	vector<int> V;
+	go(V, 1);
+	return 0;
+}
