7e9bed370803596d5bf1431e6e13e9177ae59c54
kyw04004 <kyw04004@naver.com>
2021-03-08 20:47:36 +0900

    Create 1365_꼬인 전깃줄.cpp

---
+++ BOJ/1365_꼬인 전깃줄.cpp
@@ -0,0 +1,33 @@
+#include<iostream>
+#include<cstring>
+#include<string>
+#include<algorithm>
+#include<vector>
+#include<cmath>
+#include<queue>
+#include<functional>
+#include<stack>
+#include<map>
+#include<bitset>
+using namespace std;
+typedef long long ll;
+int n, arr[100005];
+vector<int> ans;
+int main() {
+	cin.tie(NULL);
+	cout.tie(NULL);
+	ios_base::sync_with_stdio(false);
+	cin >> n;
+	for (int i = 0; i < n; i++) cin >> arr[i];
+	ans.push_back(0);
+	for (int i = 0; i < n; i++) {
+		if (ans.back() < arr[i])
+			ans.push_back(arr[i]);
+		else {
+			auto it = lower_bound(ans.begin(), ans.end(), arr[i]);
+			*it = arr[i];
+		}
+	}
+	cout << n - (ans.size() - 1) << '\n';
+	return 0;
+}
