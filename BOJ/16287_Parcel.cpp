f46dd509c8d4b9720a4c7df87b293cfd58503475
kyw04004 <kyw04004@naver.com>
2021-03-22 20:40:00 +0900

    Update 16287_Parcel.cpp

--- BOJ/16287_Parcel.cpp
+++ BOJ/16287_Parcel.cpp
@@ -8,29 +8,27 @@
 #include<functional>
 using namespace std;
 typedef long long ll;
-int w, n, arr[5001];
-vector<pair<int, int>> val[400001];
+int w, n, arr[5001], dp[400005];
 int main() {
+	cin.tie(NULL);
+	cout.tie(NULL);
+	ios_base::sync_with_stdio(false);
 	cin >> w >> n;
 	for (int i = 0; i < n; i++) cin >> arr[i];
-	for (int i = 0; i < n - 1; i++)
-		for (int j = i + 1; j < n; j++)
-			val[arr[i] + arr[j]].push_back({ i,j });
-	for (int i = 0; i < n - 1; i++)
+	for (int i = 0; i < n - 1; i++) {
 		for (int j = i + 1; j < n; j++) {
 			int now = w - arr[i] - arr[j];
-			if (now >= 0 && now<=400000 && val[now].size() >= 1) {
-				for (int k = 0; k < val[now].size(); k++)
-				{
-					if (val[now][k].first == i) continue;
-					if (val[now][k].first == j) continue;
-					if (val[now][k].second == i) continue;
-					if (val[now][k].second == j) continue;
+			if (0 <= now && now <= 400000) {
+				if (dp[now]) {
 					cout << "YES\n";
 					return 0;
 				}
 			}
 		}
+		for (int j = 0; j < i; j++)
+			if (arr[i] + arr[j] <= w)
+				dp[arr[i] + arr[j]] = 1;
+	}
 	cout << "NO\n";
 	return 0;
 }
