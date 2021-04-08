b7a57a0a4cb828348d6999189860ce594773ccc2
kyw04004 <kyw04004@naver.com>
2021-04-09 03:23:08 +0900

    Update 1911_흙길 보수하기.cpp

--- BOJ/1911_흙길 보수하기.cpp
+++ BOJ/1911_흙길 보수하기.cpp
@@ -2,24 +2,32 @@
 #include<cstring>
 #include<string>
 #include<algorithm>
+#include<map>
+#include<vector>
+#include<queue>
+#include<functional>
 using namespace std;
-pair<int, int> arr[10001];
-int main()
-{
-	int n, l, cnt, sum = 0;
-	cin >> n >> l;
-	for (int i = 0; i < n; i++)
-		scanf("%d %d", &arr[i].first, &arr[i].second);
-	sort(arr, arr + n);
-	int left = -1, right = -1;
-	for (int i = 0; i < n; i++)
-	{
-		left = max(left, arr[i].first);
-		right = max(left, max(right, arr[i].second));
-		cnt = (right - left +l - 1) / l;
-		sum += cnt;
-		left += cnt * l;
+typedef long long ll;
+typedef pair<int, int> p;
+int N, L, cnt;
+p line[10005];
+int main() {
+	cin.tie(NULL);
+	cout.tie(NULL);
+	ios_base::sync_with_stdio(false);
+	cin >> N >> L;
+	for (int i = 1; i <= N; i++)
+		cin >> line[i].first >> line[i].second;
+	sort(line + 1, line + N + 1);
+	int left = line[1].first, right = line[1].second;
+	for (int i = 2; i <= N; i++) {
+		int add = (right - left + L - 1) / L;
+		int end = left + add * L;
+		left = max(line[i].first, end);
+		right = max(line[i].second, left);
+		cnt += add;
 	}
-	printf("%d", sum);
+	cnt += (right - left + L - 1) / L;
+	cout << cnt;
 	return 0;
 }
