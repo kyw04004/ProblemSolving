f55f5d31881f50a699a42e3cc194a9a82d32997d
kyw04004 <56823099+kyw04004@users.noreply.github.com>
2021-10-28 23:05:17 +0900

    Update 1477_휴게소 세우기.cpp

--- BOJ/1477_휴게소 세우기.cpp
+++ BOJ/1477_휴게소 세우기.cpp
@@ -1,33 +1,40 @@
-#include<cstring>
-#include<string>
-#include<algorithm>
 #include<iostream>
-#include<stdio.h>
-#include<vector>
-#include<cmath>
-#include<map>
+#include <cstring>
+#include <string>
+#include <algorithm>
+#include <vector>
+#include <queue>
 using namespace std;
-typedef long long ll;
-int n, m, L, arr[105];
-int main()
-{
-	scanf("%d %d %d", &n, &m, &L);
-	for (int i = 0; i < n; i++)
-		scanf("%d", &arr[i]);
-	arr[n] = 0, arr[n + 1] = L;
-	sort(arr, arr + n + 2);
-	int left = 0, right = L;
-	while (left <= right)
-	{
-		int mid = (left + right) / 2;
-		int t = 0;
-		for (int i = 1; i < n + 2; i++)
-			t += (arr[i] - arr[i - 1] - 1) / mid;
-		if (m < t)
-			left = mid + 1;
-		else
-			right = mid - 1;
+int N, M, L, ans;
+vector<int> loc;
+bool can(int len, vector<int> loc, int m) {
+	for (int i = 0; i < loc.size() - 1; i++)
+		if (loc[i + 1] - loc[i] > len) 
+			m -= ((loc[i + 1] - loc[i] - 1) / len);
+	if (m >= 0) return true;
+	else return false;
+}
+int main(void) {
+	cin.tie(NULL);
+	cout.tie(NULL);
+	ios_base::sync_with_stdio(false);
+	cin >> N >> M >> L;
+	loc.push_back(0);
+	loc.push_back(L);
+	for (int i = 0; i < N; i++) {
+		int l; cin >> l;
+		loc.push_back(l);
 	}
-	printf("%d", left);
+	sort(loc.begin(), loc.end());
+	int left = 0; int right = L;
+	while (left <= right) {
+		int mid = (left + right) / 2;
+		if (mid == 0) mid = 1;
+		if (ans == mid) break;
+		if (can(mid, loc, M)) ans = mid, right = mid - 1;
+		else left = mid + 1;
+		if (mid == 1) break;
+	}
+	cout << ans;
 	return 0;
 }
