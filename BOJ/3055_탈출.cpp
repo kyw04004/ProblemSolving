ce271227b8151d43b36342802203c4d419032012
kyw04004 <56823099+kyw04004@users.noreply.github.com>
2022-01-03 16:08:15 +0900

    Update 3055_탈출.cpp

--- BOJ/3055_탈출.cpp
+++ BOJ/3055_탈출.cpp
@@ -1,72 +1,63 @@
 #include<iostream>
 #include<cstring>
 #include<string>
-#include<cmath>
+#include<algorithm>
+#include<vector>
+#include<functional>
 #include<queue>
+#include<stack>
 using namespace std;
-queue<pair<pair<int,int>, int>> q;
-char Map[55][55];
-int a, b;
+typedef long long ll;
+int R, C, chk[2][51][51]; // 0 : È«¼ö, 1 : µµÄ¡
+char board[51][51];
 int dx[4] = { 0,0,-1,1 };
 int dy[4] = { 1,-1,0,0 };
-int chk[55][55];
-void bfs(int x, int y, int cnt)
-{
-	chk[x][y] = 1;
-	q.push({ {x,y},0 });
-	while (!q.empty())
-	{
-		int x = q.front().first.first;
-		int y = q.front().first.second;
-		int cnt = q.front().second;
-		q.pop();
-		if (Map[x][y] == 'D')
-		{
-			printf("%d\n", cnt);
-			return;
-		}
-		if (cnt == -1)
-		{
-			for (int i = 0; i < 4; i++)
-			{
-				int nx = x + dx[i];
-				int ny = y + dy[i];
-				if (Map[nx][ny]=='X'||Map[nx][ny]=='D'||chk[nx][ny]==1||Map[nx][ny] == '*' || nx > a || nx<1 || ny>b || ny < 1) continue;
-				if (Map[nx][ny] == '.')
-				{
-					Map[nx][ny] = '*';
-					q.push({ {nx,ny},-1 });
-				}
-			}
-		}
-		else
-		{
-			for (int i = 0; i < 4; i++)
-			{
-				int nx = x + dx[i];
-				int ny = y + dy[i];
-				if (Map[nx][ny] == 'X' || chk[nx][ny] == 1 || Map[nx][ny] == '*' || nx > a || nx<1 || ny>b || ny < 1) continue;
-				chk[nx][ny] = 1;
-				q.push({ { nx,ny }, cnt + 1 });
-			}
-		}
-	}
-	printf("KAKTUS\n");
-	return;
+queue<pair<pair<int,int>, pair<int, int>>> q;
+int bfs() {
+    while (!q.empty()) {
+        int who = q.front().first.first;
+        int cnt = q.front().first.second;
+        int x = q.front().second.first;
+        int y = q.front().second.second;
+        q.pop();
+        if (who == 1 && board[x][y] == 'D') return cnt;
+        for (int i = 0; i < 4; i++) {
+            int nx = x + dx[i];
+            int ny = y + dy[i];
+            if (nx < 1 || nx > R || ny < 1 || ny > C) continue;
+            if (chk[who][nx][ny] || board[nx][ny] == 'X' || board[nx][ny] == '*') continue;
+            chk[who][nx][ny] = 1;
+            if (who == 0) {
+                if (board[nx][ny] == 'D') continue;
+                else {
+                    board[nx][ny] = '*';
+                    q.push({ {who, cnt + 1},{nx,ny} });
+                }
+            }
+            if (who == 1) q.push({ {who, cnt + 1},{nx,ny} });
+        }
+    }
+    return -1;
 }
-int main()
-{
-	cin >> a >> b;
-	for (int i = 1; i <= a; i++)
-		for (int j = 1; j <= b; j++)
-			cin >> Map[i][j];
-	for (int i = 1; i <= a; i++)
-		for (int j = 1; j <= b; j++)
-			if (Map[i][j] == '*')
-				q.push({ {i,j},-1 });
-	for (int i = 1; i <= a; i++)
-		for (int j = 1; j <= b; j++)
-			if (Map[i][j] == 'S')
-				bfs(i, j, 0);
-	return 0;
+int main() {
+    cin >> R >> C;
+    for (int i = 1; i <= R; i++)
+        for (int j = 1; j <= C; j++)
+            cin >> board[i][j];
+    for (int i = 1; i <= R; i++)
+        for (int j = 1; j <= C; j++)
+            if (board[i][j] == '*') {
+                q.push({ {0, 0 }, {i,j} });
+                chk[0][i][j] = 1;
+            }
+    for (int i = 1; i <= R; i++)
+        for (int j = 1; j <= C; j++)
+            if (board[i][j] == 'S') {
+                q.push({ {1,0}, {i,j} });
+                chk[1][i][j] = 1;
+            }
+    int ans = bfs();
+    if (ans == -1) cout << "KAKTUS";
+    else cout << ans;
+    return 0;
 }
