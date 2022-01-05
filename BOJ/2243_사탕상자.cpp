746b15b39ee0e91b2f5975e401979e1098f8c34d
kyw04004 <56823099+kyw04004@users.noreply.github.com>
2022-01-05 21:35:57 +0900

    Update 2243_사탕상자.cpp

--- BOJ/2243_사탕상자.cpp
+++ BOJ/2243_사탕상자.cpp
@@ -1,63 +1,41 @@
+#include<iostream>
+#include<map>
+#include<queue>
 #include<cstring>
 #include<string>
 #include<algorithm>
-#include<iostream>
-#include<stdio.h>
-#include<vector>
-#include<cmath>
+#include<functional>
 using namespace std;
-int h, n,m;
-vector<int> tree;
-void update(int idx, int val)
-{
-	idx += h - 1;
-	tree[idx] += val;
-	while (idx /= 2)
-		tree[idx] = tree[idx * 2] + tree[idx * 2 + 1];
+typedef long long ll;
+int n, h = 1;
+vector<int> seg;
+void update(int idx, int val) {
+	idx += (h - 1);
+	seg[idx] += val;
+	while (idx/= 2) 
+		seg[idx] = seg[2 * idx] + seg[2 * idx + 1];
 }
-int nquery(int node, int val)
-{
-	if(tree[node]>val&&node*2>=h*2)
-		return node - h + 1;
-	if (tree[node] == val)
-	{
-		while (node * 2 < h * 2)
-		{
-			if (tree[node * 2 + 1] != 0)
-			{
-				node *= 2;
-				node += 1;
-			}
-			else
-				node *= 2;
-		}
-		return node - h + 1;
-	}
-	if (val > tree[node * 2])
-		return nquery(node * 2 + 1, val - tree[node * 2]);
-	else
-		return nquery(node * 2, val);
+int nquery(int now, int val) {
+	if (now >= h) return now - h + 1;
+	if (seg[now * 2] >= val) return nquery(now * 2, val);
+	else return nquery(now * 2 + 1, val - seg[now * 2]);
 }
-int main()
-{
-	n = 1000000;
-	h = (1 << (int)log2(n - 1) + 1);
-	tree.resize(h * 2, 0);
-	scanf(" %d", &m);
-	for (int i = 1; i <= m; i++)
-	{
-		int a, b, c;
-		scanf(" %d", &a);
-		if (a == 1)
-		{
-			scanf(" %d", &b);
-			int nn = nquery(1, b);
-			printf("%d\n",nn);
-			update(nn, -1);
+int main() {
+	cin.tie(NULL);
+	cout.tie(NULL);
+	ios_base::sync_with_stdio(false);
+	cin >> n;
+	while (h < 1e6) h *= 2;
+	seg.resize(h * 2);
+	for (int i = 0; i < n; i++) {
+		int a, b, c; cin >> a >> b;
+		if (a == 1) {
+			int num = nquery(1, b);
+			cout << num << '\n';
+			update(num, -1);
 		}
-		if (a == 2)
-		{
-			scanf(" %d %d", &b, &c);
+		else {
+			cin >> c;
 			update(b, c);
 		}
 	}
