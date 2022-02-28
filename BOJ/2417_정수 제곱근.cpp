0b16eccb449b11bb8dd51bb50ed0cde24bf99c33
kyw04004 <56823099+kyw04004@users.noreply.github.com>
2022-02-28 17:24:52 +0900

    Update 2417_정수 제곱근.cpp

--- BOJ/2417_정수 제곱근.cpp
+++ BOJ/2417_정수 제곱근.cpp
@@ -1,14 +1,32 @@
 #include<iostream>
+#include<map>
+#include<queue>
 #include<cstring>
 #include<string>
 #include<algorithm>
-#include<stdio.h>
-#include<math.h>
+#include<functional>
+#include<vector>
+#include<stack>
+#include<cmath>
 using namespace std;
-int main()
-{
-	long long n;
+typedef unsigned long long ll;
+ll n, ans;
+int main() {
+	cin.tie(NULL);
+	cout.tie(NULL);
+	ios_base::sync_with_stdio(false);
 	cin >> n;
-	printf("%lld", (long long)ceil(sqrt(n)));
+	ll left = 0, right = 4e9;
+	while (left <= right) {
+		ll mid = (left + right) / 2;
+		ll num = mid * mid;
+		if (num == n) {
+			ans = mid;
+			break;
+		}
+		else if (num > n) right = mid - 1, ans = mid;
+		else left = mid + 1;
+	}
+	cout << ans;
 	return 0;
 }
